ac45ad46536e2cb472e0e0b215613e7d625205a6
EtaoinWu <etaoin.wu@gmail.com>
2022-08-07 16:00:34 +0800

    add: init ver

---
+++ base.py
@@ -0,0 +1,35 @@
+import numpy as np
+from typing import TypeVar, Generic, List, Tuple, Dict, Callable, Union, Optional, Any, Type
+
+class Proj:
+    @staticmethod
+    def __call__(v: np.ndarray) -> np.ndarray:
+        pass
+
+
+class ProjNonnegative(Proj):
+    @staticmethod
+    def __call__(v: np.ndarray) -> np.ndarray:
+        return np.maximum(v, 0)
+
+
+class ProjCube(Proj):
+    @staticmethod
+    def __call__(v: np.ndarray) -> np.ndarray:
+        return np.minimum(np.maximum(v, 0), 1)
+
+# https://arxiv.org/abs/1309.1541
+class ProjSim(Proj):
+    @staticmethod
+    def __call__(y: np.ndarray) -> np.ndarray:
+        d = y.shape[0]
+        u = np.sort(y)[::-1]
+        v = np.cumsum(u)
+        ρ = max([j for j in range(d) if u[j] + (1 - v[j]) / j > 0])
+        λ = (1 - v[ρ]) / ρ
+        return np.maximum(y + λ, 0)
+
+
+proj = ProjNonnegative()
+proj_cube = ProjCube()
+proj_sim = ProjSim()
---
+++ game.py
@@ -0,0 +1,50 @@
+import numpy as np
+from typing import TypeVar, Generic, List, Tuple, Dict, Callable, Union, Optional, Any, Type
+from abc import ABC, abstractmethod
+import string
+import torch
+from torch import nn
+
+# R^n * R^(n*m)
+GameResult = Tuple[np.ndarray, np.ndarray]
+
+class GameBase(ABC):
+    # R^(n*m) -> R^n
+    def value(self, *args: np.ndarray) -> np.ndarray:
+        return self(*args)[0]
+        
+    # R^(n*m) -> R* ^ (n*m)
+    def gradient(self, *args: np.ndarray) -> np.ndarray:
+        return self(*args)[1]
+
+    # R^(n*m) -> R^n * R* ^ (n*m)
+    @abstractmethod
+    def __call__(self, *args: np.ndarray) -> GameResult:
+        return self.value(*args), self.gradient(*args)
+
+class MultilinearGame(GameBase):
+    def __init__(self, weight: np.ndarray):
+        wt = torch.tensor(weight)
+        self.weight = wt
+        self.m = wt.shape[1]
+        self.n = wt.ndim - 1
+        assert self.n == wt.shape[0]
+        chars = string.ascii_lowercase
+        self.einsum_str = '{}{},{}->{}'.format(
+            chars[self.n], chars[:self.n], ','.join(chars[:self.n]), chars[self.n]
+        )
+        self.single_einsum_str = '{},{}'.format(
+            chars[:self.n], ','.join(chars[:self.n])
+        )
+    
+    def __call__(self, *args: np.ndarray) -> GameResult:
+        inputs = list(map(lambda x: torch.tensor(x), args))
+        outs = torch.einsum(self.einsum_str, self.weight, *inputs)
+        assert outs.shape == (self.n, )
+        l : List[np.ndarray] = []
+        for i in range(self.n):
+            g_inputs = list(map(lambda x: torch.tensor(x, requires_grad=True), args))
+            torch.einsum(self.single_einsum_str, self.weight[i], *g_inputs).backward()
+            l.append(np.array(g_inputs[i].grad.detach().numpy()))
+
+        return np.array(outs.detach().numpy()), np.array(l)
